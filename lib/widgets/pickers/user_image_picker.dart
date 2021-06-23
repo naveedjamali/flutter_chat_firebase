1e6cd9db6bf3e74011562fa94155a36aaea930a7
naveed jamali <naveedjamali@outlook.com>
2021-06-23 23:16:21 +0500

    image picker

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   const UserImagePicker({key}) : super(key: key);
@@ -8,15 +11,27 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  void _pickImage() async {
+    final pickedImageFile =
+        await ImagePicker.platform.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _pickedImage = File(pickedImageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CircleAvatar(
           radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: _pickImage,
           icon: Icon(Icons.image),
           label: Text('Add Image'),
         ),
